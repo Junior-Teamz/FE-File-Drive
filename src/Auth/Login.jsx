d2e94bbd0d83d14affa91f4c0e1d77722f4db646
Muhammad Rafi Syahbani <mrafi.syahbani@gmail.com>
2024-08-18 14:50:18 +0700

    fix logic login

--- src/Auth/Login.jsx
+++ src/Auth/Login.jsx
@@ -1,15 +1,20 @@
 import { useState } from "react";
 import {
+  Box,
   Stack,
   IconButton,
   Typography,
   InputAdornment,
   TextField,
+  Grid,
+  Container,
+  useMediaQuery,
+  useTheme,
 } from "@mui/material";
-import { LoadingButton } from "@mui/lab";
 import { EyeOff, Eye } from "lucide-react";
-import { toast } from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
+import { LoadingButton } from "@mui/lab";
+import toast from "react-hot-toast";
 import Cookies from "js-cookie";
 import Api from "../Api";
 
@@ -17,8 +22,13 @@ export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
+
+  const token = Cookies.get("token");
 
   const handleClickShowPassword = () => {
     setShowPassword((prev) => !prev);
@@ -60,7 +70,7 @@ export default function Login() {
     }
 
     try {
-      const response = await Api.post("/login", {
+      const response = await Api.post("/api/login", {
         email,
         password,
       });
@@ -103,93 +113,136 @@ export default function Login() {
       }
     } catch (error) {
       // Handle request failure
-      toast.error("Login gagal, silakan coba lagi.", {
-        position: "top-center",
-        duration: 4000,
-      });
+      if (error.response && error.response.status === 401) {
+        toast.error("Email atau password salah, silakan coba lagi.", {
+          position: "top-center",
+          duration: 4000,
+        });
+      } else {
+        toast.error("Login gagal, silakan coba lagi.", {
+          position: "top-center",
+          duration: 4000,
+        });
+      }
     } finally {
       setLoading(false);
     }
   };
 
   return (
-    <Stack
-      spacing={3}
-      sx={{
-        maxWidth: 400,
-        mx: "auto",
-        p: 4,
-        borderRadius: 2,
-        boxShadow: 3,
-        backgroundColor: "#f4f6f8",
-      }}
-      component="form"
-      onSubmit={login}
-    >
-      <Typography variant="h4" gutterBottom>
-        Welcome To Sharing File
-      </Typography>
+    <Grid container component="main" sx={{ height: "100vh" }}>
+      {!isMobile && (
+        <Grid
+          item
+          xs={12}
+          sm={6}
+          md={7}
+          sx={{
+            backgroundImage: `url(/path-to-your-image.jpg)`,
+            backgroundRepeat: "no-repeat",
+            backgroundColor: (t) =>
+              t.palette.mode === "light"
+                ? t.palette.grey[50]
+                : t.palette.grey[900],
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "100%",
+              p: 4,
+            }}
+          />
+        </Grid>
+      )}
 
-      <Typography variant="body2" color="text.secondary" mb={2}>
-        Login to your account
-      </Typography>
+      <Grid item xs={12} sm={6} md={5} component={Container} maxWidth="xs">
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            height: "100%",
+            p: 4,
+          }}
+        >
+          <Stack
+            spacing={3}
+            sx={{
+              maxWidth: 400,
+              mx: "auto",
+            }}
+            component="form"
+            onSubmit={login}
+          >
+            <Typography variant="h6" gutterBottom>
+              Welcome Back
+            </Typography>
 
-      <TextField
-        fullWidth
-        name="email"
-        label="Email"
-        variant="outlined"
-        required
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        InputProps={{
-          sx: {
-            borderRadius: "8px",
-          },
-        }}
-      />
+            <Typography variant="body2" color="text.secondary">
+              Please enter your details
+            </Typography>
 
-      <TextField
-        fullWidth
-        name="password"
-        label="Password"
-        type={showPassword ? "text" : "password"}
-        variant="outlined"
-        required
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        InputProps={{
-          endAdornment: (
-            <InputAdornment position="end">
-              <IconButton onClick={handleClickShowPassword} edge="end">
-                {showPassword ? <Eye /> : <EyeOff />}
-              </IconButton>
-            </InputAdornment>
-          ),
-          sx: {
-            borderRadius: "8px",
-          },
-        }}
-      />
+            <TextField
+              fullWidth
+              name="email"
+              label="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              InputProps={{
+                sx: {
+                  borderRadius: "8px",
+                },
+              }}
+            />
 
-      <LoadingButton
-        fullWidth
-        color="primary"
-        size="large"
-        type="submit"
-        variant="contained"
-        loading={loading}
-        sx={{
-          py: 1.5,
-          borderRadius: "8px",
-          backgroundColor: "#1976d2",
-          ":hover": {
-            backgroundColor: "#1565c0",
-          },
-        }}
-      >
-        Login
-      </LoadingButton>
-    </Stack>
+            <TextField
+              fullWidth
+              name="password"
+              label="Password"
+              type={showPassword ? "text" : "password"}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton onClick={handleClickShowPassword} edge="end">
+                      {showPassword ? <Eye /> : <EyeOff />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+                sx: {
+                  borderRadius: "8px",
+                },
+              }}
+            />
+
+            <LoadingButton
+              fullWidth
+              color="primary"
+              size="large"
+              type="submit"
+              variant="contained"
+              loading={loading}
+              sx={{
+                py: 1.5,
+                borderRadius: "8px",
+                backgroundColor: "#1976d2",
+                ":hover": {
+                  backgroundColor: "#1565c0",
+                },
+              }}
+            >
+              Login
+            </LoadingButton>
+          </Stack>
+        </Box>
+      </Grid>
+    </Grid>
   );
 }
